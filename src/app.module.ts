5dc25941177414f0598fcf424a7372f1ab2bbe1f
VitorMendonca62 <vitorqueiroz325@gmail.com>
2024-12-21 11:21:08 -0300

    (feat) adicionando leitura de variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from 'modules/task/task.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
